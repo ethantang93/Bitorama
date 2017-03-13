a0d64363705ebc457753d768c788aca9fe449c07
ethantang93 <tangrenxing@gmail.com>
2017-03-13 09:10:50 -0500

    added users model

--- apps/usersapp/models.py
+++ apps/usersapp/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.charField(max_length=30)
+    last_name = models.charField(max_length=30)
+    email = models.EmailField(max_length=30)
+    phone = models.charField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
