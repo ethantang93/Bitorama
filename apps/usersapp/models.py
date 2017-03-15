cebd04a895573adea742394ed9a07bef6729a68d
Luis Salas <salas.luis40@gmail.com>
2017-03-15 09:14:32 -0500

    finished setting up data base

--- apps/usersapp/models.py
+++ apps/usersapp/models.py
@@ -1,38 +1,37 @@
-# from __future__ import unicode_literals
-# from __future__ import unicode_literals
-# from django.contrib.auth.models import User
-# from django.db import models
-# from django.contrib.auth.models import User
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
-#
-#
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete = models.CASCADE)
-#     phone = models.CharField(max_length=30)
-#     updated_at = models.DateTimeField(auto_now=True)
-#
-# class Address(models.Model):
-#     street = models.CharField(max_length = 255)
-#     city = models.CharField(max_length = 255)
-#     state = models.CharField(max_length = 2)
-#     zip_code = models.IntegerField()
-#     apartment_num = models.CharField( max_length = 30)
-#     address_type = models.IntegerField()
-#     owners = models.ManyToManyField('User',related_name = 'addresses')
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#
-# class Message(models.Model):
-#     sender = models.ForeignKey('User',related_name ='message_sent')
-#     receiver = models.ForeignKey('User',related_name = 'message_received')
-#     content = models.CharField(max_length=1024)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#     subject = models.CharField(max_length=100)
-#
-# class Connection(models.Model):
-#     follower = models.ForeignKey('User',related_name = 'is_following')
-#     followed = models.ForeignKey('User',related_name = 'followed_by')
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+from __future__ import unicode_literals
+from django.contrib.auth.models import User
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    phone = models.CharField(max_length=30)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Address(models.Model):
+    street = models.CharField(max_length = 255)
+    city = models.CharField(max_length = 255)
+    state = models.CharField(max_length = 2)
+    zip_code = models.IntegerField()
+    apartment_num = models.CharField( max_length = 30)
+    address_type = models.IntegerField()
+    owners = models.ManyToManyField('User',related_name = 'addresses')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Message(models.Model):
+    sender = models.ForeignKey('User',related_name ='message_sent')
+    receiver = models.ForeignKey('User',related_name = 'message_received')
+    content = models.CharField(max_length=1024)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    subject = models.CharField(max_length=100)
+
+class Connection(models.Model):
+    follower = models.ForeignKey('User',related_name = 'is_following')
+    followed = models.ForeignKey('User',related_name = 'followed_by')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- apps/usersapp/views.py
+++ apps/usersapp/views.py
@@ -2,4 +2,63 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    render(request, 'usersapp/index.html')
+    return render(request,'usersapp/index.html')
+
+def register_page(request):
+    return render(request,'usersapp/register.html')
+def dashboard(request):
+    return render(request,'usersapp/dashboard.html')
+
+def login(request):
+    user = Profile.objects.validateLogin(request)
+    if (user[0]):
+        login_user(request,user[1])
+        return redirect('/users/dashboard')
+    print_messages(request,user[1])
+    return redirect('/users')
+
+def register(request):
+    username = request.POST['username']
+    email = request.POST['email']
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    password = request.POST['password']
+
+    user = Profile.objects.validateReg(request)
+    if (user[0]):
+        user = Profile.objects.create_user(username,email,password)
+        user.last_name = last_name
+        user.first_name = first_name
+        user.save()
+        login_user(request,user)
+        return redirect('/users/dashboard')
+
+    print("input is not valid")
+    print_messages(request,user[1])
+    return redirect('/users/register_page')
+
+def login_user(request,user):
+    print user
+    request.session['user'] = {
+    'first_name' : user.first_name,
+    'last_name' : user.last_name,
+    'username' : user.username,
+    'email' : user.email
+    }
+    # request.session['user'] = user
+    return redirect('/dashboard',request)
+
+    print("*"*100+"input is not valid")
+    return redirect('/users')
+
+def print_messages(request, message_list):
+    for message in message_list:
+        messages.add_message(request, messages.INFO, message)
+
+def getInfo(request):
+    print 'in the getinfo Method'
+
+    return render(request,'usersapp/user.html')
+
+def home(request):
+    return render(request, 'usersapp/home.html')
