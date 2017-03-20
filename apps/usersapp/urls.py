7408a869ebc8360d297ffacf4c8e1b31f6a7b46e
ethantang93 <tangrenxing@gmail.com>
2017-03-20 16:22:12 -0500

    added the ability to send and receive messages and to display conversation history between two users

--- apps/usersapp/urls.py
+++ apps/usersapp/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^logout$', views.logout),
     url(r'^dashboard$', views.dashboard),
     url(r'^userInfo$', views.getInfo),
-    url(r'^send/(?P<sender_id>\d+)/(?P<receiver_id>\d+)$', views.send_message),
+    url(r'^send_message/(?P<sender_id>\d+)/(?P<receiver_id>\d+)$', views.send_message),
     url(r'^del_message/(?P<message_id>\d+)$', views.delete_message),
     url(r'^follow/(?P<follower>\d+)/(?P<followed>\d+)$', views.follow),
     url(r'^unfollow/(?P<follower>\d+)/(?P<followed>\d+)$',views.unfollow)
